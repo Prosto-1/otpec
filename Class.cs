2f302f537680efea29770cbae10260283ffd592d
Prosto-1 <80521500+Prosto-1@users.noreply.github.com>
2022-06-09 14:45:04 +0300

    Add files via upload

---
+++ Class.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace otpec
+{
+   public static class Class
+    {
+        public static int id = 0;
+        public static int post = 0;
+    }
+}
